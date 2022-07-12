91fa9a92fef6add7111315eaa510f73f3812e1f2
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-12 18:24:20 +0530

    changed the code for checking depth in value

--- flatten.js
+++ flatten.js
@@ -1,15 +1,32 @@
-module.exports = function flatten(elements) {
+const items = require("./array");
+
+module.exports = function flatten(elements, depth) {
   if (!Array.isArray(elements)) {
     return [];
   } else {
-    let flattenarray = [];
-    for (let i = 0; i < elements.length; i++) {
-      if (Array.isArray(elements[i])) {
-        flattenarray = flattenarray.concat(flatten(elements[i]));
+    let nestedArray,
+      count = 0;
+    do {
+      let flattenarray = [];
+      nestedArray = false;
+      for (let i=0;i<elements.length;i++) {
+        if (Array.isArray(elements[i])) {
+          flattenarray.push(...elements[i]);
+          nestedArray = true;
+        } else if (elements[i] === undefined) {
+          continue;
+        } else {
+          flattenarray.push(elements[i]);
+        }
+      } 
+      count++;
+      elements = flattenarray;
+      if (count === depth || !nestedArray) {
+        break;
       } else {
-        flattenarray.push(elements[i]);
+        continue;
       }
-    }
-    return flattenarray;
+    } while (nestedArray);
+    return elements;
   }
 };
