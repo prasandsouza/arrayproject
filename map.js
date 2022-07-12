2b6e14832118c3ea7c3b824d3ed55f8af80cedf2
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-12 13:44:31 +0530

    changes the code for empty array

--- map.js
+++ map.js
@@ -1,8 +1,11 @@
 module.exports = function map(elements, cb) {
-  if (!elements || !cb) return elements;
-  let numberArray = [];
-  for (let i = 0; i < elements.length; i++) {
-    numberArray.push(cb(elements[i], i));
+  if (!Array.isArray(elements)) {
+    return [];
+  } else {
+    let numberArray = [];
+    for (let i = 0; i < elements.length; i++) {
+      numberArray.push(cb(elements[i], i));
+    }
+    return numberArray;
   }
-  return numberArray;
 };
