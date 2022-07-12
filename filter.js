e1a9f6f55ea48fb719b422820a1c0a723cd8817b
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-12 18:25:36 +0530

    modified elements to check return

--- filter.js
+++ filter.js
@@ -2,12 +2,10 @@ module.exports = function filter(elements, callback) {
   if (!Array.isArray(elements)) {
     return [];
   } else {
-    let numberArray = [];
     for (let i = 0; i < elements.length; i++) {
       if (callback(elements[i])) {
-        numberArray.push(callback(elements[i], i));
+        return callback(elements[i], i);
       }
     }
-    return numberArray;
   }
 };
