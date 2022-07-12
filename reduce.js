9b044a105eb2a861badfa62b2cd946f1d738f8f9
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-12 13:44:56 +0530

    changes the code for empty array

--- reduce.js
+++ reduce.js
@@ -1,8 +1,11 @@
 module.exports = function reduce(elements, cb, startingValue) {
-  if (!elements || !cb || !startingValue) return [];
-  for (let i = 0; i < elements.length; i++) {
-    let accumulator = cb(startingValue, elements[i]);
-    startingValue = accumulator;
+  if (!Array.isArray(elements)) {
+    return [];
+  } else {
+    for (let i = 0; i < elements.length; i++) {
+      let accumulator = cb(startingValue, elements[i]);
+      startingValue = accumulator;
+    }
+    return startingValue;
   }
-  return startingValue;
 };
