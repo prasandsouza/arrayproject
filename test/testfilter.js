681cba78b355bacd326e12733346d45072219a27
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-13 15:35:06 +0530

    changed code for coding hygine

--- test/testfilter.js
+++ test/testfilter.js
@@ -1,9 +1,13 @@
-let array = require(`../array`)
-let logic = require(`../filter.js`)
+let array = require(`../array`);
+let logic = require(`../filter.js`);
 
-let callback = function(number,index,array){
-    if(number%2===0) return true
-}
+let callback = function (number, index, array) {
+  if (number % 2 === 0){
+    return true;
+  }else{
+    return false;
+  }
+};
 
-let result = logic(array,callback)
-console.log(result)
+let result = logic(array, callback);
+console.log(result);
