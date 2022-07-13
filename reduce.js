dc9055ad85f48bc63785632ac9ac25a62dbbc702
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-13 18:40:39 +0530

    updated code for starting value

--- reduce.js
+++ reduce.js
@@ -4,7 +4,8 @@ module.exports = function reduce(elements, callback, startingValue) {
     }
     if (startingValue === undefined) {
       startingValue = elements[0];
-    }   
+    }
+    else startingValue = callback(startingValue,elements[0],elements)   
     for (let index = 1; index < elements.length; index++) {
       let items = elements[index];
       startingValue = callback(startingValue, items,index, elements);
