9665b156ad766ebb7aba9981c9e312f1603d6852
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-13 13:02:39 +0530

    changing code for error in reduce while returning

--- reduce.js
+++ reduce.js
@@ -1,10 +1,17 @@
 module.exports = function reduce(elements, callback, startingValue) {
-  if (!Array.isArray(elements)||!callback||!startingValue) {
+  if (!Array.isArray(elements) || !callback || !startingValue) {
     return [];
-  } else {
-    for (let i = 1; i < elements.length; i++) {
-     startingValue = callback(startingValue,elements[i],elements)
-    }
-    return startingValue;
   }
+  if(startingValue === undefined){
+    startingValue = elements[i]
+  }
+  else{
+    startingValue = callback(startingValue,elements[0],elements)
+  }
+
+  for (let index = 1; index < elements.length; index++) {
+    let items = elements[index]
+    startingValue = callback(startingValue, items, elements);
+  }
+  return startingValue;
 };
