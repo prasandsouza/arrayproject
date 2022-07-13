7901f9af4ecb46d956830ef56103aff41e8cd21f
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-13 13:51:10 +0530

    changed the code for starting value

--- test/testreduce.js
+++ test/testreduce.js
@@ -1,8 +1,8 @@
 let array = require(`../array`);
 let logic = require(`../reduce`);
-let startingValue = array[0];
+let startingValue
 let callback = function (startingValue, elements) {
-  return startingValue * elements;
+  return startingValue + elements;
 };
 let result = logic(array, callback, startingValue);
 console.log(result);
