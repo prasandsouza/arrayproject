8c2f8d19f4b0dc92807338d83f56764287aed78f
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-13 14:50:57 +0530

    passed fourth values as arguments

--- reduce.js
+++ reduce.js
@@ -2,16 +2,15 @@ module.exports = function reduce(elements, callback, startingValue) {
   if (!Array.isArray(elements) || !callback) {
     return [];
   }
-  if(startingValue === undefined){
-    startingValue = elements[0]
-  }
-  else{
-    startingValue = callback(startingValue,elements[0],elements)
+  if (startingValue === undefined) {
+    startingValue = elements[0];
+  } else {
+    startingValue = callback(startingValue, elements[0], elements);
   }
 
   for (let index = 1; index < elements.length; index++) {
-    let items = elements[index]
-    startingValue = callback(startingValue, items, elements);
+    let items = elements[index];
+    startingValue = callback(startingValue, items, index, elements);
   }
   return startingValue;
 };
