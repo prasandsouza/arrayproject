450c8f19ee3a802469ef0d3a8f6f59f1d26c7fcf
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-13 13:13:01 +0530

    changed the entire code for flatten because of depth operation

--- flatten.js
+++ flatten.js
@@ -1,32 +1,15 @@
-const items = require("./array");
-
 module.exports = function flatten(elements, depth) {
-  if (!Array.isArray(elements)) {
-    return [];
-  } else {
-    let nestedArray,
-      count = 0;
-    do {
-      let flattenarray = [];
-      nestedArray = false;
-      for (let i=0;i<elements.length;i++) {
-        if (Array.isArray(elements[i])) {
-          flattenarray.push(...elements[i]);
-          nestedArray = true;
-        } else if (elements[i] === undefined) {
-          continue;
-        } else {
-          flattenarray.push(elements[i]);
-        }
-      } 
-      count++;
-      elements = flattenarray;
-      if (count === depth || !nestedArray) {
-        break;
-      } else {
-        continue;
-      }
-    } while (nestedArray);
-    return elements;
+  if (depth === undefined) {
+    depth = 1;
   }
+  let flatarray = [];
+  for (let index = 0; index < elements.length; index++) {
+    if (Array.isArray(elements[index]) && depth > 0) {
+      flatarray = flatarray.concat(flatten(elements[index], depth - 1));
+    } else if (elements[index] === undefined) continue;
+    else {
+      flatarray.push(elements[index]);
+    }
+  }
+  return flatarray;
 };
