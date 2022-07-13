ed953d172ca37bd83472e26950c495580a6ac2b6
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-13 15:33:21 +0530

    added fourth element

--- test/testreduce.js
+++ test/testreduce.js
@@ -1,7 +1,7 @@
 let array = require(`../array`);
 let logic = require(`../reduce`);
 let startingValue;
-let callback = function (startingValue, index, elements) {
+let callback = function (startingValue, index, elements,array) {
   return startingValue + index;
 };
 let result = logic(array, callback, startingValue);
