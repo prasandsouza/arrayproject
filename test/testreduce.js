5f3c70e5cd65bdb5f1b4d153d37a0115d1dbf0bd
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-13 14:50:10 +0530

    passed third values as arguments

--- test/testreduce.js
+++ test/testreduce.js
@@ -1,8 +1,9 @@
 let array = require(`../array`);
 let logic = require(`../reduce`);
-let startingValue
-let callback = function (startingValue, elements) {
+let startingValue;
+let callback = function (startingValue, index, array) {
   return startingValue + elements;
 };
 let result = logic(array, callback, startingValue);
 console.log(result);
+
